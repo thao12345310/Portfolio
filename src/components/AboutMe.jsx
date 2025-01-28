ae1f029d124cb258eb9db67eae19d795646b65d9
Thao <foreveralone2111997@gmail.com>
2025-01-28 20:33:26 +0700

    basic start

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm [Your Name], a passionate web developer with a love for creating
+        beautiful and functional websites. I specialize in ReactJS, JavaScript.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a>About Me</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
